814df4b29fc8b1d5c4b25dc697b649393c3c132d
Sriyanti <sriy49063@gmail.com>
2025-09-28 09:11:49 +0700

    update bin/latihan dart 1

--- bin/latihan_dart_1.dart
+++ bin/latihan_dart_1.dart
@@ -1,5 +1,5 @@
-import 'package:latihan_dart_1/latihan_dart_1.dart' as latihan_dart_1;
+
 
 void main(List<String> arguments) {
-  print('Hello world: ${latihan_dart_1.calculate()}!');
+  print('Hello world latihan');
 }
