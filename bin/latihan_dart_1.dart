b14c3e038c017cb3db165d129d45ebb4f706b7a0
Sriyanti <sriy49063@gmail.com>
2025-09-28 09:27:44 +0700

    perubahan code

--- bin/latihan_dart_1.dart
+++ bin/latihan_dart_1.dart
@@ -2,4 +2,15 @@
 
 void main(List<String> arguments) {
   print('Hello world latihan');
+
+  var fullName = 'Dart Programming';
+  var age = 1;
+  var height = 1.6;
+  var isSingle = true;
+
+  print(fullName);
+  print(age);
+  print(height);
+  print(isSingle);
 }
+
